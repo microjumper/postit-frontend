458126542a5b3edee5addb781ff1fa049f3e9a49
Salvatore Sammarco <microjmpr@gmail.com>
2021-04-20 17:28:16 +0200

    primeng

---
+++ src/app/modules/primeng.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RippleModule } from 'primeng/ripple';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    RippleModule,
+  ]
+})
+export class PrimengModule { }
